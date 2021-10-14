6bd0fe5a79c8eba93bbf29dd5571c79df6c1b76b
Gyasi Igyan <gyasiigyan13@gmail.com>
2021-10-14 12:43:10 -0400

    okay

--- studentTable.php
+++ studentTable.php
@@ -1,5 +1,17 @@
 <?php
-// PHP Data Objects(PDO) Sample Code:
-echo "Welcome";
+
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$database = "STUDENTS"; 
+
+// $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $database);
+// if ( mysqli_connect_errno() ) {
+// 	// If there is an error with the connection, stop the script and display the error.
+// 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+// }
+
+
+
 
 ?>
